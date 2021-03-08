65e283c967c639af2023d0dc62f735e2ef36af77
Quentin Vaillant <quentin095.vaillant@gmail.com>
2021-03-08 21:57:55 +0100

    create room template

--- assets/room.js
+++ assets/room.js
@@ -1,11 +1,24 @@
-const eventSource = new EventSource('http://127.0.0.1/.well-known/mercure?topic=' + encodeURIComponent('http://example.com/books/1'));
+
+import './styles/room.scss';
+import './styles/fonts/ionicons.min.css';
+import './styles/fonts/fontawesome-all.min.css';
+
+const eventSource = new EventSource('http://127.0.0.1:2700/.well-known/mercure?topic=' + encodeURIComponent('http://example.com/books/1'));
 eventSource.onmessage = event => {
     // Will be called every time an update is published by the server
     console.log(JSON.parse(event.data)); //['answer']
     // $("body").append("<h1>"+JSON.parse(event.data)['question']+"</h1>");
 }
-window.addEventListener('beforeunload', function(){
-    if(eventSource != null){
-        eventSource.close()
-    }
-})
+
+// window.addEventListener('beforeunload', function(){
+//     if(eventSource != null){
+//         eventSource.close()
+//     }
+// })
+
+(function($) {
+    $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+        $("body").toggleClass("sidebar-toggled");
+        $(".sidebar").toggleClass("toggled");
+    });
+})(jQuery);
