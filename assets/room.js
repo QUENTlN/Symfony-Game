a2f23dc63129c7f77e66c65e6a06ae64a86225e7
Quentin Vaillant <quentin095.vaillant@gmail.com>
2021-03-21 15:45:49 +0100

    link home with rooms

--- assets/room.js
+++ assets/room.js
@@ -1,24 +1,236 @@
-
 import './styles/room.scss';
 import './styles/fonts/ionicons.min.css';
 import './styles/fonts/fontawesome-all.min.css';
 
-const eventSource = new EventSource('http://127.0.0.1:2700/.well-known/mercure?topic=' + encodeURIComponent('http://example.com/books/1'));
-eventSource.onmessage = event => {
-    // Will be called every time an update is published by the server
-    console.log(JSON.parse(event.data)); //['answer']
-    // $("body").append("<h1>"+JSON.parse(event.data)['question']+"</h1>");
-}
 
-// window.addEventListener('beforeunload', function(){
-//     if(eventSource != null){
-//         eventSource.close()
-//     }
-// })
+const playerId = $('#js-get-id-player').data('playerId');
+const username = $('#js-get-username').data('username');
+const roomId = $('#js-get-id-room').data('roomId');
+const hostId = $('#js-get-id-host').data('hostId');
+const nbRound = $('#js-get-nb-round').data('nbRound');
+let alreadyCorrectAnswer = false;
 
-(function($) {
-    $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+(function ($) {
+    $("#sidebarToggle, #sidebarToggleTop").on('click', function (e) {
         $("body").toggleClass("sidebar-toggled");
         $(".sidebar").toggleClass("toggled");
     });
 })(jQuery);
+
+const _receiver = document.getElementById('mercure-content-receiver');
+const _messageInput = document.getElementById('mercure-message-input');
+const _sendForm = document.getElementById('mercure-message-form');
+const _startForm = document.getElementById('start-game-form');
+const _nextQuestionForm = document.getElementById('next-question-form');
+const _answerForm = document.getElementById('answer-form');
+const _callAnswerForm = document.getElementById('call-answer-form');
+
+
+if (hostId === playerId && _startForm !== null) {
+    _startForm.onsubmit = event => {
+        event.preventDefault();
+        sendMessage('startGame');
+        $('#start-game-form').hide()
+        fetch(_startForm.action, {
+            method: _startForm.method
+        }).then(() => {
+        });
+        return false;
+    }
+}
+
+const sendMessage = (typeMsg) => {
+    fetch(_sendForm.action, {
+        method: _sendForm.method,
+        body: 'type=' + typeMsg + '&idUser=' + playerId + '&idRoom=' + roomId + '&username=' + username,
+        headers: new Headers({
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        })
+    }).then(() => {
+        _messageInput.value = '';
+    });
+};
+
+_sendForm.onsubmit = (evt) => {
+    sendMessage(_messageInput.value);
+    evt.preventDefault();
+    return false;
+};
+
+const nextQuestion = () => {
+    let currentRound = $('#current-round').val();
+    if (currentRound <= nbRound) {
+        fetch(_nextQuestionForm.action, {
+            method: _nextQuestionForm.method,
+            body: 'current=' + currentRound + '&room=' + roomId,
+            headers: new Headers({
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            })
+        }).then(() => {
+            $('#current-round').val(parseInt(currentRound) + 1)
+        });
+    } else {
+        sendMessage('showResult');
+    }
+}
+
+const startQuestionChrono = () => {
+    var timeleft = 15;
+    var downloadTimer = setInterval(function () {
+        if (timeleft <= 0) {
+            clearInterval(downloadTimer);
+            document.getElementById("time-addon").innerHTML = "0";
+        } else {
+            document.getElementById("time-addon").innerHTML = timeleft;
+        }
+        timeleft -= 1;
+    }, 1000);
+}
+
+const url = new URL('http://127.0.0.1:2700/.well-known/mercure');
+url.searchParams.append('topic', 'http://mercure.hub/room/'.concat(roomId));
+const eventSource = new EventSource(url);
+eventSource.onmessage = event => {
+    const data = JSON.parse(event.data);
+    switch (data.type) {
+        case 'answer':
+            if (data.isCorrect === true) {
+                $('#score-user-' + data.idUser).html(parseInt($('#score-user-' + data.idUser).html()) + parseInt(data.newPoints));
+            } else {
+                if (parseInt(data.idUser) === playerId) {
+                    $('#answer-input').prop('disabled', false);
+                }
+            }
+            let scores = []
+            $(".user-info").each(function (index) {
+                scores[index] = {
+                    id: $(this).data('idPlayer'),
+                    score: $(this).find(".score").text(),
+                    pseudo: $(this).find(".pseudo").text()
+                }
+            })
+            scores.sort((a, b) => parseFloat(b.score) - parseFloat(a.score));
+            $("#accordionSidebar").html("")
+            $.each(scores, function (index, user) {
+                $("#accordionSidebar").append("\n" +
+                    "                        <li class=\"nav-item\" id=\"div-user-{{ score.guest.id }}\">\n" +
+                    "                            <div class=\"nav-link active user-info\" data-id-player=\"" + user.id + "\">\n" +
+                    "                                <i class=\"icon ion-happy\"></i>\n" +
+                    "                                <span class=\"pseudo\">" + user.pseudo + "</span>\n" +
+                    "                                <span id=\"score-user-" + user.id + "\"\n" +
+                    "                                      class=\"badge badge-primary float-right score\">" + user.score + "</span>\n" +
+                    "                            </div>\n" +
+                    "                        </li>")
+            });
+            break;
+        case 'join':
+            if ($("#div-user-" + data.idUser).length === 0) {
+                $("#accordionSidebar").append('<li class="nav-item" id="div-user-' + data.idUser + '">\n' +
+                    '                        <div class="nav-link active">\n' +
+                    '                            <i class="icon ion-happy"></i>\n' +
+                    '                            <span>' + data.username + '</span>\n' +
+                    '                            <span class="badge badge-primary float-right">0</span>\n' +
+                    '                        </div>\n' +
+                    '                    </li>');
+            }
+            break;
+        case 'pushQuestion':
+            $('#answerDiv').removeAttr('hidden')
+            $('#answer-input').removeAttr('disabled');
+            switch (data.questionType) {
+                case 'QuestionWithText':
+                    $('#question-content').html('<h1 class="h1 text-center mx-auto d-block w-auto unselectable">' + data.text + '</h1>')
+                    $('#question-content-generated').html('')
+                    break;
+                case 'QuestionWithPicture':
+                    $('#question-content').html('<img class="rounded mx-auto d-block w-auto unselectable" \n' +
+                        '                                 src="./../games_images/guess_the/44cafe894c519dc4595f2c4c47a6997c.jpg' /*+ data.link*/ + '" \n' +
+                        '                                 style="max-height: 70vh; max-width: 100%" alt="?Guess The?"> ')
+                    $('#question-content-generated').html('De quel ' + data.subcategory + ' est tirée cette image ?')
+                    break;
+            }
+            startQuestionChrono()
+            _answerForm.onsubmit = event => {
+                event.preventDefault();
+                $("#answer-input").prop('disabled', true);
+                let message = $("#answer-input").val()
+                $("#answer-input").val("");
+                fetch(_answerForm.action, {
+                    method: _answerForm.method,
+                    body: 'type=answer&message=' + message + '&idRoom=' + roomId + '&time=' + parseInt($('#time-addon').html()) + '&question=' + data.idQuestion + '&idUser=' + playerId,
+                    headers: new Headers({
+                        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                    })
+                }).then(() => {
+                });
+            }
+            if (hostId === playerId) {
+                setTimeout(function () {
+                    fetch(_callAnswerForm.action, {
+                        method: _callAnswerForm.method,
+                        body: 'type=pushAnswer&idRoom=' + roomId + '&question=' + data.idQuestion,
+                        headers: new Headers({
+                            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                        })
+                    }).then(() => {
+                        setTimeout(function () {
+                            nextQuestion()
+                        }, 5000);
+                    });
+                }, 15000);
+            }
+            break;
+        case 'pushAnswer':
+            $('#question-content').html('<h1 class="h1 text-center mx-auto d-block w-auto unselectable">' + data.answer + '</h1>');
+            $('#question-content-generated').html('');
+            $('#answer-input').prop('disabled', true);
+            $('#answer-input').val('');
+            break;
+        case 'startGame':
+            $('#waiting-text').hide();
+            $('#gameCard').removeClass("bg-gradient-primary");
+            if (hostId === playerId) {
+                nextQuestion();
+                $("#replay-game").remove();
+            }
+            break;
+        case 'showResult':
+            $("#answerDiv").attr("hidden", true);
+            if (hostId === playerId) {
+                $("#answerDiv").before("<div id=\"replay-game\" class=\"row w-100 justify-content-center\" >\n" +
+                    "                    <button class=\"btn btn-primary btn-lg\">Recommencer</button>\n" +
+                    "                </div>")
+            }
+            $("#question-content").html("\n" +
+                "                                <table class=\"table text-center w-50 mx-auto border-top-0\">\n" +
+                "                                    <tbody class=\"text-light\">\n" +
+                "                                    <tr class=\"\">\n" +
+                "                                        <th scope=\"col\" style=\"border-top-left-radius: 15px\">#</th>\n" +
+                "                                        <th scope=\"col\">Joueur</th>\n" +
+                "                                        <th scope=\"col\" style=\"border-top-right-radius: 15px\">Score</th>\n" +
+                "                                    </tr>\n" +
+                "                                    </tbody>\n" +
+                "                                </table>")
+            let position = 1;
+            $(".user-info").each(function () {
+                let userInfo = this
+                $("tbody").append("\n" +
+                    "                                    <tr>\n" +
+                    "                                        <th scope=\"row\">" + position + "</th>\n" +
+                    "                                        <td>" + $(userInfo).find(".pseudo").text() + "</td>\n" +
+                    "                                        <td>" + $(userInfo).find(".score").text() + "</td>\n" +
+                    "                                    </tr>")
+                position++
+            })
+            $("#gameCard").addClass('bg-gradient-primary');
+            break;
+    }
+    if (!data.message) {
+        return;
+    }
+    _receiver.insertAdjacentHTML('beforeend', `<div class="message">${data.message}</div>`);
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+    sendMessage('join');
+});
--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -4,58 +4,226 @@ namespace App\Controller;
 
 use App\Entity\Player;
 use App\Entity\Room;
+use App\Entity\Game;
 use App\Entity\RoomSettings;
-use App\Entity\SubCategory;
-use App\Repository\RoomSettingsRepository;
-use App\Form\ConfigEnregistreType;
+use App\Entity\Score;
 use App\Form\CreateRoomType;
-use App\Repository\SubCategoryRepository;
+use App\Repository\AnswerRepository;
+use App\Repository\GuestRepository;
+use App\Repository\QuestionRepository;
+use App\Repository\RoomRepository;
+use App\Repository\RoundRepository;
+use App\Repository\ScoreRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mercure\PublisherInterface;
 use Symfony\Component\Mercure\Update;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RoomController extends AbstractController
 {
     /**
      * @Route("/quentin", name="quentin")
-     * @param PublisherInterface $publisher
+     * @param MessageBusInterface $bus
+     * @param Request $request
+     * @return RedirectResponse
      */
-    public function quentin(PublisherInterface $publisher): Response
+    public function quentin(MessageBusInterface $bus, Request $request): RedirectResponse
     {
         $update = new Update(
-            'http://example.com/books/1',
-            json_encode(['status' => 'OutOfStock'])
+            'http://mercure.hub/room/' . $request->request->get('idRoom'),
+            json_encode([
+                'type' => $request->request->get('type'),
+                'message' => $request->request->get('message'),
+                'idUser' => $request->request->get('idUser'),
+                'username' => $request->request->get('username')
+            ])
         );
-
-        $publisher($update);
-
-        return new Response('published!');
+        $bus->dispatch($update);
+        return $this->redirectToRoute('room', ['id' => $request->request->get('idRoom')]);
     }
 
     /**
-     * @Route("/room", name="room")
+     * @Route("/pushAnswer", name="pushAnswer")
+     * @param MessageBusInterface $bus
+     * @param AnswerRepository $answerRepository
+     * @param QuestionRepository $questionRepository
+     * @param Request $request
+     * @return RedirectResponse
      */
-    public function index(): Response
+    public function pushAnswer(MessageBusInterface $bus, AnswerRepository $answerRepository, QuestionRepository $questionRepository, Request $request): RedirectResponse
     {
-        return $this->render('room/index.html.twig', [
-            'controller_name' => 'RoomController',
-        ]);
+        $question = $questionRepository->findOneBy(['id' => $request->request->get('question')]);
+        $answer = $answerRepository->findOneBy(['question' => $question]);
+        $update = new Update(
+            'http://mercure.hub/room/' . $request->request->get('idRoom'),
+            json_encode([
+                'type' => 'pushAnswer',
+                'answer' => $answer->getTextAnswer()
+            ])
+        );
+        $bus->dispatch($update);
+        return $this->redirectToRoute('room', ['id' => $request->request->get('idRoom')]);
+    }
+
+    /**
+     * @Route("/answerQuestion", name="answerQuestion")
+     * @param MessageBusInterface $bus
+     * @param EntityManagerInterface $entityManager
+     * @param Request $request
+     * @param ScoreRepository $scoreRepository
+     * @param GuestRepository $guestRepository
+     * @param QuestionRepository $questionRepository
+     * @param RoomRepository $roomRepository
+     * @param AnswerRepository $answerRepository
+     * @return RedirectResponse
+     */
+    public function answerQuestion(MessageBusInterface $bus, EntityManagerInterface $entityManager, Request $request, ScoreRepository $scoreRepository, GuestRepository $guestRepository, QuestionRepository $questionRepository, RoomRepository $roomRepository, AnswerRepository $answerRepository): RedirectResponse
+    {
+        $question = $questionRepository->findOneBy(['id' => $request->request->get('question')]);
+        $answer = $answerRepository->findOneBy(['question' => $question, 'textAnswer' => $request->request->get('message')]);
+        if ($answer !== null) {
+            $newPoints = $request->request->get('time');
+
+            $body = json_encode([
+                'type' => $request->request->get('type'),
+                'isCorrect' => true,
+                'message' => $request->request->get('message'),
+                'idUser' => $request->request->get('idUser'),
+                'newPoints' => $newPoints
+            ]);
+        } else {
+            $body = json_encode([
+                'type' => $request->request->get('type'),
+                'isCorrest' => false,
+                'message' => $request->request->get('message'),
+                'idUser' => $request->request->get('idUser')
+            ]);
+        }
+        $update = new Update(
+            'http://mercure.hub/room/' . $request->request->get('idRoom'),
+            $body
+        );
+        $bus->dispatch($update);
+        if ($answer !== null) {
+            $room = $roomRepository->findOneBy(['id' => $request->request->get('idRoom')]);
+            $guest = $guestRepository->findOneBy(['id' => $request->request->get('idUser')]);
+            $score = $scoreRepository->findOneBy(['guest' => $guest, 'room' => $room]);
+
+            $score->setScore($score->getScore() + $newPoints);
+            $entityManager->persist($score);
+            $entityManager->flush();
+        }
+        return $this->redirectToRoute('room', ['id' => $request->request->get('idRoom')]);
+    }
+
+    /**
+     * @Route("/nextQuestion", name="nextQuestion")
+     * @param MessageBusInterface $bus
+     * @param Request $request
+     * @param RoundRepository $roundRepository
+     * @param RoomRepository $roomRepository
+     * @return RedirectResponse
+     */
+    public function nextQuestion(MessageBusInterface $bus, Request $request, RoundRepository $roundRepository, RoomRepository $roomRepository): RedirectResponse
+    {
+
+        $room = $roomRepository->findOneBy(['id' => $request->request->get('room')]);
+        $round = $roundRepository->findOneBy(['room' => $room, 'indexOrder' => $request->request->get('current')]);
+        $question = $round->getQuestion();
+        if ($question->getType() === 'QuestionWithText') {
+            $update = new Update(
+                'http://mercure.hub/room/' . $request->request->get('room'),
+                json_encode([
+                    'type' => 'pushQuestion',
+                    'game' => $question->getSubCategory()->getCategory()->getGame()->getName(),
+                    'subcategory' => $question->getSubCategory()->getLibSubCategory(),
+                    'text' => $question->getText(),
+                    'idQuestion' => $question->getId(),
+                    'questionType' => $question->getType()
+                ])
+            );
+        } else {
+            $update = new Update(
+                'http://mercure.hub/room/' . $request->request->get('room'),
+                json_encode([
+                    'type' => 'pushQuestion',
+                    'game' => $question->getSubCategory()->getCategory()->getGame()->getName(),
+                    'subcategory' => $question->getSubCategory()->getLibSubCategory(),
+                    'link' => $question->getLinkPicture(),
+                    'idQuestion' => $question->getId(),
+                    'questionType' => $question->getType()
+                ])
+            );
+        }
+        $bus->dispatch($update);
+        return $this->redirectToRoute('room', ['id' => $request->request->get('room')]);
+    }
+
+
+    /**
+     * @Route("/startGame/{id}", name="startGame", requirements={"id"="\d+"})
+     * @param int $id
+     * @param EntityManagerInterface $manager
+     * @param RoomRepository $roomRepository
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function startGame(int $id, EntityManagerInterface $manager, RoomRepository $roomRepository, Request $request): RedirectResponse
+    {
+        $room = $roomRepository->findOneBy(['id' => $id, 'finishedAt' => null]);
+        $room->setStartedAt(new \DateTime());
+        $manager->persist($room);
+        $manager->flush();
+
+        return $this->redirectToRoute('room', ['id' => $id]);
+    }
+
+
+    /**
+     * @Route("/room/{id}", name="room", requirements={"id"="\d+"})
+     * @param int $id
+     * @param EntityManagerInterface $manager
+     * @param RoomRepository $roomRepository
+     * @param ScoreRepository $scoreRepository
+     * @return Response
+     */
+    public function index(int $id, EntityManagerInterface $manager, RoomRepository $roomRepository, ScoreRepository $scoreRepository): Response
+    {
+        $room = $roomRepository->findOneBy(['id' => $id, 'finishedAt' => null]);
+        if ($room === null) {
+            //signal error
+        } else {
+            $score = $scoreRepository->findOneBy(['guest' => $this->getUser(), 'room' => $id]);
+            if ($score === null) {
+                $score = new Score;
+                $score->setRoom($room);
+                $score->setGuest($this->container->get('security.token_storage')->getToken()->getUser());
+                $score->setScore(0);
+                $manager->persist($score);
+                $manager->flush();
+            }
+            return $this->render('room/index.html.twig', [
+                'room' => $room,
+            ]);
+        }
     }
 
 
     /**
      * @Route("/create_room/{access}", name="createRoom" , methods={"GET","POST"})
+     * @param Request $request
+     * @param string $access
+     * @param EntityManagerInterface $entityManager
+     * @return Response
      */
     public function new(Request $request, string $access, EntityManagerInterface $entityManager): Response
     {
         $room = new Room();
-        if ($access == 'private') {
+        if ($access === 'private') {
             $room->setIsPrivate(true);
         } else {
             $room->setIsPrivate(false);
@@ -83,7 +251,6 @@ class RoomController extends AbstractController
             $roomSetting->setNameProfil(null);
             $entityManager->persist($room);
             $entityManager->flush();
-            dd($room);
             return $this->redirectToRoute('room',['id'=> $room->getId()]);
         }
 
@@ -100,6 +267,8 @@ class RoomController extends AbstractController
 
     /**
      * @Route("/search_param", name="searchParam" , methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
      */
     public function searchParam(Request $request): Response
     {
